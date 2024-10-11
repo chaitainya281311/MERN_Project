ff160c396e5cc782ddceb25295bbd41fdab27d3c
chaitainya281311 <chaitanyatavate51@gmail.com>
2024-10-11 20:49:34 +0530

    server

---
+++ seedData.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const mongoose = require('mongoose');
+const Transaction = require('./model/transaction'); 
+
+const mongoURI = 'mongodb://localhost:27017/transaction'; 
+
+mongoose.connect(mongoURI, {
+  serverSelectionTimeoutMS: 50000
+})
+  .then(() => {
+    console.log('MongoDB connected');
+    fetchData();
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+const fetchData = async () => {
+  try {
+    await Transaction.deleteMany();
+    const { data } = await axios.get('https://s3.amazonaws.com/roxiler.com/product_transaction.json');
+    console.log('Fetched data:', data.length, 'items');
+    
+    await Transaction.insertMany(data);
+    console.log("Data Inserted Successfully!");
+  } catch (error) {
+    console.log('Error during data insertion:', error.message);
+  }
+};
+
+fetchData();
